a5bde2e229a7eb1237881653c9512d95fe0072a2
isyankarcoder <gencvolkan@gmail.com>
2020-12-20 14:41:29 +0300

    ..

---
+++ HelloWorld2/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	var x float64=10.8;	
+	var y float64=9.87;
+
+	z:= x+y;
+
+	fmt.Println(z);
+	fmt.Println(x);
+	fmt.Println(y);
+    
+	fmt.Println("volki tolki");
+}

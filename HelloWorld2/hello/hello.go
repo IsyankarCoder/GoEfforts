2b83d7ef133259b20cabf7e03fdfde7a46174621
Isyankarcoder <you@example.com>
2021-02-20 22:04:37 +0300

    go 1.16

--- HelloWorld2/hello/hello.go
+++ HelloWorld2/hello/hello.go
@@ -2,57 +2,55 @@ package main
 
 import "fmt"
 
-
-func oddoreven(){
-	if 7%2==0 {
-	   fmt.Println("Tek")
-	} else{
-		fmt.Println("Çift")}
+func oddoreven() {
+	if 7%2 == 0 {
+		fmt.Println("Tek")
+	} else {
+		fmt.Println("Çift")
 	}
-
-func hasdigit(){
-	if num := 9; num < 0 {
-        fmt.Println(num, "is negative")
-    } else if num < 10 {
-        fmt.Println(num, "has 1 digit")
-    } else {
-        fmt.Println(num, "has multiple digits")
-    }
+	//
 }
 
-func main(){
-	var x float64=10.8;	
-	var y float64=9.87;
+func hasdigit() {
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+}
 
-	z:= x+y;
+func main() {
+	var x float64 = 10.8
+	var y float64 = 9.87
 
-	fmt.Println(z);
-	fmt.Println(x);
-	fmt.Println(y);
-	
-	
-	d:=&x;
+	z := x + y
 
-	fmt.Println(d);
-	fmt.Println(*d);
-	fmt.Println("volki tolki");
+	fmt.Println(z)
+	fmt.Println(x)
+	fmt.Println(y)
 
-	var k, l int =1,2;
-	fmt.Println(k,l);
+	d := &x
 
+	fmt.Println(d)
+	fmt.Println(*d)
+	fmt.Println("volki tolki")
 
-   o:=1;
-   for o <=3 {
-	   fmt.Println(o);
-	   o++; 
-	 }
-	
+	var k, l int = 1, 2
+	fmt.Println(k, l)
 
-	for u:=1;u<=3; u++{
-		fmt.Println(u);
+	o := 1
+	for o <= 3 {
+		fmt.Println(o)
+		o++
 	}
 
-	oddoreven();
-	hasdigit();
+	for u := 1; u <= 3; u++ {
+		fmt.Println(u)
+	}
+
+	oddoreven()
+	hasdigit()
 
 }
